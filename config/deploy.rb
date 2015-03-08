913380eb2666bddcb9a27fdf472c06afc5d91c10
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-08 20:07:26 +0100

    refs #1 Rename application to Murdoch and don't symlink application.yml anymore.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:gawlista/#{application}.git"
+set :repository, "git@github.com:volontarian/murd.ch.git"
 set :branch, "master"
 set :rake, "#{rake} --trace"
 set :bundle_flags, '--deployment'
@@ -38,7 +38,6 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   
